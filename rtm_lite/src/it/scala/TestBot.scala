013e5c314070cbf84c85c6ac113748e226f5d6e9
Anthony Garo <agaro1121@gmail.com>
2018-07-08 18:35:02 -0400

    updates test bot

--- rtm_lite/src/it/scala/TestBot.scala
+++ rtm_lite/src/it/scala/TestBot.scala
@@ -2,7 +2,7 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import com.github.agaro1121.rtmlite
 import com.github.agaro1121.rtmlite.client.AbilityToRespondToRtm
-import com.github.agaro1121.sharedevents.models.{Message, SlackMessage}
+import com.github.agaro1121.sharedevents.models.SlackMessage
 
 object TestBot {
   def props: Props = Props(new TestBot())
@@ -10,9 +10,6 @@ object TestBot {
 
 class TestBot extends AbilityToRespondToRtm {
   override def receiveWithWsActorToRespond(slack: ActorRef): Receive = {
-
-    case msg@Message(_, _, _, "hi2", _, _, _) =>
-      slack ! msg.replyWithMessage(s"Test Bot received message: hi2")
 
     case msg@SlackMessage(text, _, _, _, _) =>
       println(s"Test Bot received Abstract message: $text")
